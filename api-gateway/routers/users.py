c6b1a2819f65fa187bd944ce8f03ce09ccbf856e
Pablo Retamales <retamalespj@gmail.com>
2025-11-22 20:47:14 -0300

    fix: user router

--- api-gateway/routers/users.py
+++ api-gateway/routers/users.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
 from utils.forward import forward
 from config import URLS
 
@@ -12,22 +14,34 @@ async def gw_users_health(request: Request):
 # Registrar usuario
 @router.post("")
 async def gw_users_create(request: Request):
-    body = await request.json()
-    return await forward("POST", f"{URLS['users']}/v1/users/register", request, body=body)
+    try:
+        body = await request.json()
+    except:
+        body = None
+
+    return await forward("POST", f"{URLS['users']}/usersservice/v1/users/register", request, body=body)
 
 # Login
 @router.post("/login")
 async def gw_users_login(request: Request):
-    body = await request.json()
-    return await forward("POST", f"{URLS['users']}/v1/auth/login", request, body=body)
+    try:
+        body = await request.json()
+    except:
+        body = None
+
+    return await forward("POST", f"{URLS['users']}/usersservice/v1/auth/login", request, body=body)
 
 # Leer usuario autenticado
 @router.get("/me")
 async def gw_users_me(request: Request):
-    return await forward("GET", f"{URLS['users']}/v1/users/me", request)
+    return await forward("GET", f"{URLS['users']}/usersservice/v1/users/me", request)
 
 # Actualizar perfil propio
 @router.patch("/me")
 async def gw_users_update_me(request: Request):
-    body = await request.json()
-    return await forward("PATCH", f"{URLS['users']}/v1/users/me", request, body=body)
+    try:
+        body = await request.json()
+    except:
+        body = None
+
+    return await forward("PATCH", f"{URLS['users']}/usersservice/v1/users/me", request, body=body)
